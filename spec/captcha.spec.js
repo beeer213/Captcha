9a0f1df8570f841d1cba24026529154c27aa173e
Bigbeer <Bigbeer>
2016-10-10 23:05:51 +0700

    Add function Opera toString()

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,5 +1,21 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-});
+function Captcha(pattern, operator, leftOperand, rightOperand){
+    this.generate = function() {
+        let op = new opera(operator);
+    }
+  }
+function opera(o){
+  this.toString = function(){
+    if(o===1){
+      return '+';
+    }
+    else if(o===2){
+      return '-';
+    }
+    if(o==='3'){
+      return '*';
+    }
+    if(o==='4'){
+      return '/';
+    }
+  }
+}
